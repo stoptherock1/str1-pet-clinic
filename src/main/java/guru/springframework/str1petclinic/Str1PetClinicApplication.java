7f3d02cae0927391b49f982b419cf29b3b6b7010
str1 <stoptherock1@gmail.com>
2020-03-29 18:25:09 +1100

    Init repository commit

---
+++ src/main/java/guru/springframework/str1petclinic/Str1PetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.str1petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Str1PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Str1PetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/str1petclinic/Str1PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.str1petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Str1PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
