58f6943e09564884f369732d222f557fa27a998b
stoptherock1 <stoptherock1@gmail.com>
2020-07-26 00:49:42 +1000

    Add Map-based services
    Closes #5

---
+++ pet-clinic-data/src/main/java/org/str1/str1petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package org.str1.str1petclinic.services.map;
+
+import org.str1.str1petclinic.model.Pet;
+import org.str1.str1petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
