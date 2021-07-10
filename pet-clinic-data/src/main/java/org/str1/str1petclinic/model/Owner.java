ec03f4f135ee190703f8c293393314c98f4dc030
Mikalai Barysau <stoptherock1@gmail.com>
2021-07-10 16:23:52 +1000

    Create Pet Type, Pet, and Visit Entities
    Closes #26

--- pet-clinic-data/src/main/java/org/str1/str1petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/org/str1/str1petclinic/model/Owner.java
@@ -1,4 +1,15 @@
 package org.str1.str1petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+    Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
