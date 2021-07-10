b9b031ed8884c75e625902741b46f40a0ce94de7
Mikalai Barysau <stoptherock1@gmail.com>
2021-07-10 16:35:25 +1000

    Add Specialties
    Closes #27

--- pet-clinic-data/src/main/java/org/str1/str1petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/str1/str1petclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package org.str1.str1petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
+
+    private Set<Specialty> specialties;
 }
